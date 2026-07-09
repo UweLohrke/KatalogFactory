a25b834372ad0e1f7acf771376c509614813b2d8
Uwe Lohrke <lohrke.uwe@gmail.com>
2026-07-09 21:41:34 +0200

    Version 0.5.0 - Projektdokumentation

--- src/catalog/catalog_builder.py
+++ src/catalog/catalog_builder.py
@@ -1,13 +1,43 @@
+"""
+KatalogFactory
+
+Datei:
+catalog_builder.py
+
+Version:
+0.5.0
+
+Beschreibung:
+Erzeugt einen nach Herstellern gruppierten Katalog.
+Die Artikel bleiben zunächst als Pandas-Zeilen erhalten.
+Die Umstellung auf Article-Objekte erfolgt in einer späteren Version.
+"""
+
 from collections import defaultdict
 
 
 class CatalogBuilder:
 
     def build(self, dataframe):
+        """
+        Erstellt einen Katalog nach Herstellern.
+
+        Rückgabe:
+        {
+            "BAUCK": [Artikel1, Artikel2, ...],
+            "BIOVEGAN": [Artikel1, Artikel2, ...]
+        }
+        """
+
         katalog = defaultdict(list)
 
         for _, zeile in dataframe.iterrows():
+
             hersteller = str(zeile["Zusatztext"]).strip()
+
+            if not hersteller:
+                hersteller = "Ohne Hersteller"
+
             katalog[hersteller].append(zeile)
 
         return dict(sorted(katalog.items()))
